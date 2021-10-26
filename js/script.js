6550280487efc2300ef5f4eb2712e9ee0b548b0f
Agata Jarmoszuk <agnes365@vp.pl>
2021-10-26 18:25:37 +0200

    Add script

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var computerMove, playermove;
+computerMove = 'kamień';
+playermove = 'print';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+printMessage(playermove);
